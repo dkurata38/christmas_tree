4a02382f98a90fea7d73fb091e5785324241588b
KJMAN <koji.smz@gmail.com>
2018-12-09 17:28:21 +0900

    Create sc.py
    スクレイピング用

---
+++ christmas_tree_ml/sc.py
@@ -0,0 +1,20 @@
+import requests # urlを読み込むためrequestsをインポート
+from bs4 import BeautifulSoup # htmlを読み込むためBeautifulSoupをインポート
+ 
+URL = 'https://amanaimages.com/keyword/result.aspx?Page=Result&KeyWord=%89%CE%92%8C&DispTurn=0&DispThumb=130&PageNum=1' # URL入力
+images = [] # 画像リストの配列
+ 
+soup = BeautifulSoup(requests.get(URL).content,'lxml') # bsでURL内を解析
+ 
+for link in soup.find_all("img"): # imgタグを取得しlinkに格納
+    if link.get("src").endswith(".jpg"): # imgタグ内の.jpgであるsrcタグを取得
+        images.append(link.get("src")) # imagesリストに格納
+    elif link.get("src").endswith(".png"): # imgタグ内の.pngであるsrcタグを取得
+    	images.append(link.get("src")) # imagesリストに格納
+ 
+for target in images: # imagesからtargetに入れる
+    re = requests.get(target)
+    with open('img/' + target.split('/')[-1], 'wb') as f: # imgフォルダに格納
+        f.write(re.content) # .contentにて画像データとして書き込む
+ 
+print("ok") # 確認
